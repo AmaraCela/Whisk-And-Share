86e238d426552044a33f7e48434d3896fdc67b1d
AmaraCela <amaracela@gmail.com>
2023-10-10 22:33:50 +0200

    home page init

--- whiskAndShare/base/views.py
+++ whiskAndShare/base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+recipes = [
+    {"title":'Cheesecake', "body":'lorem ipsum'},
+    {"title":'Hashbrowns', "body":'lorem ipsum'}
+
+]
+
+def home(request):
+    context = {'recipes':recipes}
+    return render(request, 'base/home.html', context)
